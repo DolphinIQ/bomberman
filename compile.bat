07a6b35c7b267f849b4d1e86f0adb68f966e9f02
DolphinIQ <piotr.stylinski@gmail.com>
2025-07-29 02:01:25 +0200

    Fix batch file typo

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 SET compiler=gcc
-@REM %compiler% -Wall -Wextra -g src/main_win32.c -o bomberman | /.bomberman
-gcc -Wall -Wextra -ggdb src/main_win32.c -l"gdi32" -l"winmm" -o bomberman | /.bomberman
+@REM %compiler% -Wall -Wextra -g src/main_win32.c -o bomberman | ./bomberman
+gcc -Wall -Wextra -ggdb src/main_win32.c -l"gdi32" -l"winmm" -o bomberman | ./bomberman
 
 @REM bomberman
 
